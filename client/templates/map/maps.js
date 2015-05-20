033724e8d8aa451379e136a222c129c12e078742
typeofgraphic <pbooth80@gmail.com>
2015-05-20 14:21:10 +0100

    meteor setup

---
+++ client/templates/map/maps.js
@@ -0,0 +1,23 @@
+Template.map.helpers({
+
+
+});
+
+Template.map.events({
+
+
+
+});
+
+
+
+Template.map.rendered = function(){
+
+  var map = L.map('map').setView([51.505, -0.09], 13);
+
+  var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+};
